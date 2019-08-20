7b576bb56a3bcb3e6d9e04db58b3c97ce4edcf81
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-08-20 23:59:10 +0100

    feat/add active menu functionality

---
+++ public/asset/js/master.js
@@ -0,0 +1,12 @@
+(function(){
+
+    //set active menu 
+    const activeMenu = () => {
+        const path = window.location.pathname;
+        $('.sidebar-fixed a').removeClass('active')
+        $(`.sidebar-fixed a[href="${path}"]`).addClass('active');
+    }
+
+    activeMenu();
+
+}());
